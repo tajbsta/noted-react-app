9d4259a8d2a4eb93ea86b695d70279b70405c633
Breech Oracion <breech@outliant.com>
2021-02-23 19:31:45 +0800

    feat: initial join conversion

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,38 +1,110 @@
-import React from 'react';
-import { Mail } from 'react-feather';
+import React from "react";
+import { LogIn, Mail } from "react-feather";
+import GoogleLogin from "react-google-login";
+import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
   return (
     <div>
       <div classNameName="container">
         <div className="row justify-content-center">
           <div className="col-12 col-md-5 col-xl-4 my-5">
-            <h1 className="display-4 text-center mb-3">Sign in</h1>
             <p className="text-center">Need to return or donate</p>
             <p className="text-center">purchases made in the past?</p>
             <p className="text-center">Let's go!</p>
-            <form>
-              <div className="form-group">
-                <input type="email" className="form-control" placeholder="Your email..." />
+            <form className="mb-3">
+              <GoogleLogin
+                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                render={(renderProps) => (
+                  <div
+                    className="form-group"
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <button
+                      className="btn btn-md btn-block"
+                      style={{
+                        background: "white",
+                        boxShadow: "0px 0px 4px 0.5px rgba(0,0,0,0.1)",
+                        color: "#690098",
+                        fontWeight: "normal",
+                      }}
+                    >
+                      <div className="avatar avatar-xs mr-2">
+                        <img
+                          className="avatar-img"
+                          src="https://i.pinimg.com/originals/39/21/6d/39216d73519bca962bd4a01f3e8f4a4b.png"
+                        />
+                      </div>
+                      Join With Google
+                    </button>
+                  </div>
+                )}
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+              />
+              <div
+                className="form-group"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <button
+                  className="btn btn-md btn-block"
+                  style={{
+                    background: "white",
+                    boxShadow: "0px 0px 4px 0.5px rgba(0,0,0,0.1)",
+                    color: "#690098",
+                    fontWeight: "normal",
+                  }}
+                >
+                  <div className="avatar avatar-xs mr-2">
+                    <img
+                      className="avatar-img"
+                      src="https://i.pinimg.com/originals/39/21/6d/39216d73519bca962bd4a01f3e8f4a4b.png"
+                    />
+                  </div>
+                  Join With Apple
+                </button>
               </div>
-              <button className="btn btn-lg btn-block btn-primary mb-3">
-                <i className="fe fe-mail">
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Your email..."
+                />
+              </div>
+              <button className="btn btn-lg btn-block btn-success mb-3">
+                <i className="fe fe-mail mr-3">
                   <Mail />
                 </i>
                 Join with email
               </button>
               <div className="text-center">
                 <small className="text-muted text-center">
-                  By joining Noted you agree to our Terms of Service and Privacy. Protected by
-                  Google's Privacy and Terms.
+                  By joining Noted you agree to our Terms of Service and
+                  Privacy. Protected by Google's Privacy and Terms.
                 </small>
-                <div>
-                  <h3>
-                    Already a member? <a href="#">Log in</a>
-                  </h3>
-                </div>
               </div>
             </form>
+            <h3>
+              Already a member?{" "}
+              <Link
+                to="login"
+                className="text-success text-decoration-underline"
+                style={{}}
+              >
+                Log in
+              </Link>
+            </h3>
           </div>
         </div>
       </div>
