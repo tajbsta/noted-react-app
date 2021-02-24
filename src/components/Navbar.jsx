bcce18e3b6059b0872230d7c715dba7bfafa8c35
Breech Oracion <breech@outliant.com>
2021-02-24 22:42:56 +0800

    feat: added sofia and search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React, { lazy } from "react";
 import { Container, Navbar } from "react-bootstrap";
-
 const BrandLogoSvg = lazy(() => import("./BrandLogoSvg"));
 
 const Topnav = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar
+      expand="lg shadow-sm"
+      style={{
+        background: "white",
+      }}
+    >
       <Container>
         <Navbar.Brand href="home">
           <BrandLogoSvg />
         </Navbar.Brand>
+
+        <input
+          type="email"
+          className="form-control ml-4 search"
+          aria-describedby="emailHelp"
+          placeholder="Search purchases"
+        />
       </Container>
     </Navbar>
   );
