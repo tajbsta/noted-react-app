59e693c7d815af8041f69e3a9d4018497d203bbb
Breech Oracion <breech@outliant.com>
2021-02-25 18:44:59 +0800

    fix: components js to jsx

---
+++ src/components/Dashboard/Scanning.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Scanning() {
+  return (
+    <>
+      <p className="text-center sofia-pro noted-purple">Scan running...</p>
+      <p className="small text-muted mb-1 text-center sofia-pro">
+        Go have some coffee - we&apos;ll email ya when it&apos;s done!
+      </p>
+    </>
+  );
+}
+
+export default Scanning;
