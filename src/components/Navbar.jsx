68618605f74b8aad1b2cfe0eb73625b388ce8991
Janica Dino <janicadino@gmail.com>
2021-03-08 17:22:58 +0800

    add logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,17 +67,47 @@ const Topnav = () => {
               placeholder="Search purchases"
             />
           </Container>
-          <div className="row select-dropdown">
-            <div className="col-6">
-              <button className="btn p-0" onClick={logout}>
-                <img src={ProfileIcon} />
-              </button>
-            </div>
-            <div className="col-6">
-              <div className="btn p-0">
-                <img src={DropwDownIcon} />
-              </div>
-            </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbar-list-4"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbar-list-4">
+            <ul className="navbar-nav">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <img src={ProfileIcon} width="25" height="25" />
+                </a>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  {/* <a className="dropdown-item" href="#">
+                    Dashboard
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Edit Profile
+                  </a> */}
+                  <button className="dropdown-item sofia-pro" onClick={logout}>
+                    Log Out
+                  </button>
+                </div>
+              </li>
+            </ul>
           </div>
         </>
       )}
