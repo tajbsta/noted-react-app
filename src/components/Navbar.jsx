bc68c459042f4dba51649f5465c2171f1201b23b
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-03-10 14:37:29 +0800

    fix: fixed logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import React, { lazy } from "react";
-import { Container, Navbar } from "react-bootstrap";
-import ProfileIcon from "../assets/icons/Profile.svg";
-import DropwDownIcon from "../assets/icons/InvertedTriangle.svg";
-import Search from "../assets/icons/Search.svg";
-import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { Auth } from "aws-amplify";
-import { unsetUser } from "../actions/auth.action";
-import { get } from "lodash";
+import React, { lazy } from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+import ProfileIcon from '../assets/icons/Profile.svg';
+import DropwDownIcon from '../assets/icons/InvertedTriangle.svg';
+import Search from '../assets/icons/Search.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
+import { unsetUser } from '../actions/auth.action';
+import { get } from 'lodash';
 
-const BrandLogoSvg = lazy(() => import("./BrandLogoSvg"));
+const BrandLogoSvg = lazy(() => import('./BrandLogoSvg'));
 
 const Topnav = () => {
   let history = useHistory();
@@ -17,59 +17,62 @@ const Topnav = () => {
   const pageLocation = history.location.pathname;
 
   const guestViews = [
-    "/",
-    "/login",
-    "/join",
-    "/forgot-password",
-    "/reset-password",
-    "/request-permission/",
-    "/request-permission",
-    "/code",
-    "/code/",
+    '/',
+    '/login',
+    '/join',
+    '/forgot-password',
+    '/reset-password',
+    '/request-permission/',
+    '/request-permission',
+    '/code',
+    '/code/',
   ];
   const {
     location: { pathname },
   } = useHistory();
   const user = useSelector(({ auth: { user } }) => user);
-  const showShadow = guestViews.includes(pathname) ? "" : "shadow-sm";
+  const showShadow = guestViews.includes(pathname) ? '' : 'shadow-sm';
 
   const logout = async () => {
-    try {
-      await Auth.signOut();
-      dispatch(unsetUser());
-      history.push("/login");
-    } catch (error) {
-      console.log("Error Signing Out: ", error);
-    }
+    dispatch(await unsetUser());
+    Auth.signOut()
+      .then(async (data) => {
+        if (!user) {
+          history.push('/');
+        }
+      })
+      .catch((error) => {
+        console.log('Error Signing Out: ', error);
+      });
   };
 
   return (
     <Navbar
       expand={`lg ${showShadow}`}
       style={{
-        border: "none",
-        backgroundColor: guestViews.includes(pathname) ? "#FAF8FA" : "",
+        border: 'none',
+        backgroundColor: guestViews.includes(pathname) ? '#FAF8FA' : '',
       }}
     >
       <Navbar.Brand
-        href={`${guestViews.indexOf(pageLocation) != -1 ? "/" : "/dashboard"}`}
-        className="ml-4 mr-1"
+        href={`${guestViews.indexOf(pageLocation) != -1 ? '/' : '/dashboard'}`}
+        className='ml-4 mr-1'
       >
         <BrandLogoSvg />
       </Navbar.Brand>
-      {["/dashboard", "/view-scan"].includes(pathname) && (
+      {['/dashboard', '/view-scan'].includes(pathname) && (
         <>
-          <Container className="ml-3">
+          <Container className='ml-3'>
             <form>
-              <div className="input-group input-group-lg input-group-merge background-color">
+              <div className='input-group input-group-lg input-group-merge background-color'>
                 <input
-                  type="text"
-                  className="form-control form-control-prepended list-search background-color sofia-pro text-16 color"
-                  placeholder="Search purchases"
+                  type='text'
+                  className='form-control form-control-prepended list-search background-color sofia-pro text-16 color'
+                  placeholder='Search purchases'
                 />
-                <div className="input-group-prepend">
-                  <div className="input-group-text background-color">
-                    <span className="fe fe-search">
+                <div className='input-group-prepend'>
+                  <div className='input-group-text background-color'>
+                    <span className='fe fe-search'>
                       <img src={Search} />
                     </span>
                   </div>
@@ -84,33 +87,33 @@ const Topnav = () => {
             /> */}
           </Container>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbar-list-4"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbar-list-4'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbar-list-4">
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
+          <div className='collapse navbar-collapse' id='navbar-list-4'>
+            <ul className='navbar-nav'>
+              <li className='nav-item dropdown'>
                 <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
+                  className='nav-link dropdown-toggle'
+                  href='#'
+                  id='navbarDropdownMenuLink'
+                  role='button'
+                  data-toggle='dropdown'
+                  aria-haspopup='true'
+                  aria-expanded='false'
                 >
-                  <img src={ProfileIcon} width="30" height="30" />
+                  <img src={ProfileIcon} width='30' height='30' />
                 </a>
                 <div
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
+                  className='dropdown-menu'
+                  aria-labelledby='navbarDropdownMenuLink'
                 >
                   {/* <a className="dropdown-item" href="#">
                     Dashboard
@@ -118,7 +121,7 @@ const Topnav = () => {
                   <a className="dropdown-item" href="#">
                     Edit Profile
                   </a> */}
-                  <button className="dropdown-item sofia-pro" onClick={logout}>
+                  <button className='dropdown-item sofia-pro' onClick={logout}>
                     Log Out
                   </button>
                 </div>
