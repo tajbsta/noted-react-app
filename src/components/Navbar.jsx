88d05d9248d4dc5b00ef0b2c0ac57df68f894b4c
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-03-13 01:49:30 +0800

    feat: added realtime search from temporarily from local storage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import React, { lazy } from "react";
-import { Container, Navbar } from "react-bootstrap";
-import ProfileIcon from "../assets/icons/Profile.svg";
-import DropwDownIcon from "../assets/icons/InvertedTriangle.svg";
-import Search from "../assets/icons/Search.svg";
-import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { Auth } from "aws-amplify";
-import { unsetUser } from "../actions/auth.action";
-import { unsetScan } from "../actions/scans.action";
+import React, { lazy } from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+import ProfileIcon from '../assets/icons/Profile.svg';
+import DropwDownIcon from '../assets/icons/InvertedTriangle.svg';
+import Search from '../assets/icons/Search.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
+import { unsetUser } from '../actions/auth.action';
+import { unsetScan } from '../actions/scans.action';
+import { searchScans } from '../actions/runtime.action';
+import { get, isEmpty } from 'lodash';
 
-import { get } from "lodash";
-
-const BrandLogoSvg = lazy(() => import("./BrandLogoSvg"));
+const BrandLogoSvg = lazy(() => import('./BrandLogoSvg'));
 
 const Topnav = () => {
   let history = useHistory();
@@ -19,21 +19,24 @@ const Topnav = () => {
   const pageLocation = history.location.pathname;
 
   const guestViews = [
-    "/",
-    "/login",
-    "/join",
-    "/forgot-password",
-    "/reset-password",
-    "/request-permission/",
-    "/request-permission",
-    "/code",
-    "/code/",
+    '/',
+    '/login',
+    '/join',
+    '/forgot-password',
+    '/reset-password',
+    '/request-permission/',
+    '/request-permission',
+    '/code',
+    '/code/',
   ];
   const {
     location: { pathname },
   } = useHistory();
-  const username = useSelector(({ auth: { username } }) => username);
-  const showShadow = guestViews.includes(pathname) ? "" : "shadow-sm";
+  const { username, scans } = useSelector(({ auth: { username }, scans }) => ({
+    username,
+    scans,
+  }));
+  const showShadow = guestViews.includes(pathname) ? '' : 'shadow-sm';
 
   const logout = async () => {
     dispatch(await unsetUser());
@@ -41,76 +44,85 @@ const Topnav = () => {
     Auth.signOut()
       .then(async (data) => {
         setTimeout(() => {
-          history.push("/login");
+          history.push('/login');
         }, 400);
       })
       .catch((error) => {
-        console.log("Error Signing Out: ", error);
+        console.log('Error Signing Out: ', error);
       });
+  };
+
+  const submitSearch = (keyword) => {
+    dispatch(searchScans(keyword));
+  };
+
+  const onSearchSubmit = (e) => {
+    if (e.keyCode === 13 && !isEmpty(e.target.value)) {
+      submitSearch(e.target.value);
+    }
   };
 
   return (
     <Navbar
       expand={`lg ${showShadow}`}
       style={{
-        border: "none",
-        backgroundColor: guestViews.includes(pathname) ? "#FAF8FA" : "",
+        border: 'none',
+        backgroundColor: guestViews.includes(pathname) ? '#FAF8FA' : '',
       }}
     >
       <Navbar.Brand
-        href={`${guestViews.indexOf(pageLocation) != -1 ? "/" : "/dashboard"}`}
-        className="ml-4 mr-1"
+        href={`${guestViews.indexOf(pageLocation) != -1 ? '/' : '/dashboard'}`}
+        className='ml-4 mr-1'
       >
         <BrandLogoSvg />
       </Navbar.Brand>
-      {["/dashboard", "/view-scan"].includes(pathname) && (
+      {['/dashboard', '/view-scan'].includes(pathname) && (
         <>
-          <Container className="ml-3">
-            <form>
-              <div className="input-group input-group-lg input-group-merge background-color">
-                <input
-                  type="text"
-                  className="form-control form-control-prepended list-search background-color sofia-pro text-16 color"
-                  placeholder="Search purchases"
-                />
-                <div className="input-group-prepend">
-                  <div className="input-group-text background-color">
-                    <span className="fe fe-search">
-                      <img src={Search} />
-                    </span>
-                  </div>
+          <Container className='ml-3'>
+            <div className='input-group input-group-lg input-group-merge background-color'>
+              <input
+                type='text'
+                className='form-control form-control-prepended list-search background-color sofia-pro text-16 color'
+                placeholder='Search purchases'
+                onChange={(e) => submitSearch(e.target.value)}
+              />
+              <div className='input-group-prepend'>
+                <div className='input-group-text background-color'>
+                  <span className='fe fe-search'>
+                    <img src={Search} />
+                  </span>
                 </div>
               </div>
-            </form>
+            </div>
           </Container>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbar-list-4"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbar-list-4'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbar-list-4">
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
+          <div className='collapse navbar-collapse' id='navbar-list-4'>
+            <ul className='navbar-nav'>
+              <li className='nav-item dropdown'>
                 <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
+                  className='nav-link dropdown-toggle'
+                  href='#'
+                  id='navbarDropdownMenuLink'
+                  role='button'
+                  data-toggle='dropdown'
+                  aria-haspopup='true'
+                  aria-expanded='false'
                 >
-                  <img src={ProfileIcon} width="30" height="30" />
+                  <img src={ProfileIcon} width='30' height='30' />
                 </a>
                 <div
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
+                  className='dropdown-menu'
+                  aria-labelledby='navbarDropdownMenuLink'
                 >
                   {/* <a className="dropdown-item" href="#">
                     Dashboard
@@ -118,7 +130,7 @@ const Topnav = () => {
                   <a className="dropdown-item" href="#">
                     Edit Profile
                   </a> */}
-                  <button className="dropdown-item sofia-pro" onClick={logout}>
+                  <button className='dropdown-item sofia-pro' onClick={logout}>
                     Log Out
                   </button>
                 </div>
