6e1531c520563aea1a8e39e46ec9ad521eef1a18
Janica Dino <janicadino@gmail.com>
2021-03-17 17:19:10 +0800

    remove isSecured

--- src/constants/privateRoutes.js
+++ src/constants/privateRoutes.js
@@ -9,17 +9,14 @@ export default [
   {
     path: '/request-permission',
     component: AuthorizePage,
-    isSecured: true,
   },
   {
     path: '/dashboard',
     component: DashboardPage,
-    isSecured: true,
   },
   {
     path: '/view-scan',
     component: ViewScanPage,
-    isSecure: true,
   },
-  { path: '/scanning', component: ScanningPage, isSecured: true },
+  { path: '/scanning', component: ScanningPage },
 ];
