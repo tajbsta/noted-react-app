c15e3632c1a8507ed4b6d32f25b5c5734d4feeae
Janica Dino <janicadino@gmail.com>
2021-03-22 02:17:23 +0800

    added Cancel Order Modal

--- src/components/Dashboard/modals/ReturnPolicyModal.jsx
+++ src/components/Dashboard/modals/ReturnPolicyModal.jsx
@@ -1,27 +1,26 @@
-import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 export default function ReturnPolicyModal(props) {
   return (
     <Modal
       {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
       centered
-      backdrop="static"
+      backdrop='static'
       keyboard={false}
-      modalOptions={{ dismissible: false }}
-      id="ReturnPolicyModal"
+      id='ReturnPolicyModal'
     >
       <Modal.Header>
-        <Modal.Title id="contained-modal-title-vcenter">
+        <Modal.Title id='contained-modal-title-vcenter'>
           Return Policy
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body className="sofia-pro">
-        <h4 className="sofia-pro brand">Nordstrom</h4>
-        <h4 className="sofia-pro text-score">Excellent Returns</h4>
-        <p className="sofia-pro info">
+      <Modal.Body className='sofia-pro'>
+        <h4 className='sofia-pro brand'>Nordstrom</h4>
+        <h4 className='sofia-pro text-score'>Excellent Returns</h4>
+        <p className='sofia-pro info'>
           Nordstrom returns are free, all the time. Due to increase volume and
           the impact of COVID-19, returns processing by mail is delayed. For
           faster returns, visit a Nordstrom store near you. For employee safety,
@@ -29,10 +28,10 @@ export default function ReturnPolicyModal(props) {
           should be expected 5-7 business days after the returns processing is
           complete.
         </p>
-        <a className="sofia-pro view-link">View website</a>
+        <a className='sofia-pro view-link'>View website</a>
       </Modal.Body>
       <Modal.Footer>
-        <Button className="btn-ok" onClick={props.onHide}>
+        <Button className='btn-ok' onClick={props.onHide}>
           OK, Got it
         </Button>
       </Modal.Footer>
