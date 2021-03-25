c0f85714803139442149f20419cc411b1069c962
Janica Dino <janicadino@gmail.com>
2021-03-25 20:37:49 +0800

    remove lazy in privateRoutes

--- src/constants/privateRoutes.js
+++ src/constants/privateRoutes.js
@@ -1,15 +1,19 @@
-import { lazy } from 'react';
 import AuthorizePage from '../pages/AuthorizePage';
 import ProfilePage from '../pages/ProfilePage';
 import SettingsPage from '../pages/SettingsPage';
+import DashboardPage from '../pages/DashboardPage';
+import ViewScanPage from '../pages/ViewScanPage';
+import ScanningPage from '../pages/ScanningPage';
+import ViewConfirmedReturnPage from '../pages/ViewConfirmedReturn';
+import EditOrderPage from '../pages/EditOrder';
 
-const DashboardPage = lazy(() => import('../pages/DashboardPage'));
-const ViewScanPage = lazy(() => import('../pages/ViewScanPage'));
-const ScanningPage = lazy(() => import('../pages/ScanningPage'));
-const ViewConfirmedReturnPage = lazy(() =>
-  import('../pages/ViewConfirmedReturn')
-);
-const EditOrderPage = lazy(() => import('../pages/EditOrder'));
+// const DashboardPage = lazy(() => import('../pages/DashboardPage'));
+// const ViewScanPage = lazy(() => import('../pages/ViewScanPage'));
+// const ScanningPage = lazy(() => import('../pages/ScanningPage'));
+// const ViewConfirmedReturnPage = lazy(() =>
+//   import('../pages/ViewConfirmedReturn')
+// );
+// const EditOrderPage = lazy(() => import('../pages/EditOrder'));
 
 export default [
   { path: '/request-permission', component: AuthorizePage },
