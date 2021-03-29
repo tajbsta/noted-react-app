d761caaf7851764fc96568cda04e38c3e6a43a47
Janica Dino <janicadino@gmail.com>
2021-03-29 22:40:10 +0800

    delete account section

---
+++ src/components/Settings/DeleteAccount.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+export default function DeleteAccount() {
+  return (
+    <div id='DeleteAccount'>
+      <div className='mt-5'>
+        <h3 className='sofia-pro text-18 mb-4'>Delete Account</h3>
+
+        <div className='card shadow-sm mb-2 w-840 change-container'>
+          <div className='card-body'>
+            <Row>
+              <Col>
+                <h4>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Quisque pharetra consequat cursus ac auctor duis commodo
+                  pulvinar.
+                </h4>
+              </Col>
+              <Col className='d-flex justify-content-center'>
+                <button className='btn btn-delete'>Delete Account</button>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
