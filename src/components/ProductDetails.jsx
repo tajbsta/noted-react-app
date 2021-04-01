9528d6ff8b9b051fb7ec800e853bb228d1e572cc
breechoracion <breech@outliant.com>
2021-04-02 00:30:29 +0800

    feat: Added 'Donate Instead' button on product card hover

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,35 +1,42 @@
-import React from "react";
-import Row from "./Row";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import Row from './Row';
+import { useHistory } from 'react-router-dom';
 
 function ProductDetails({
   scannedItem: { itemName, vendorTag, amount, compensationType },
+  isHovering = false,
 }) {
   const history = useHistory();
   const pageLocation = history.location.pathname;
-  const orderViews = ["/view-return", "/view-scan"];
+  const orderViews = ['/view-return', '/view-scan'];
 
   return (
     <div
       className={`col-sm-7 p-0 mt-1 p-details ${
-        orderViews.indexOf(pageLocation) != -1 ? "scheduled-height" : ""
+        orderViews.indexOf(pageLocation) != -1 ? 'scheduled-height' : ''
       }`}
     >
       <Row>
-        <h4 className="mb-0 sofia-pro mb-1 distributor-name">{vendorTag}</h4>
+        <h4 className='mb-0 sofia-pro mb-1 distributor-name'>{vendorTag}</h4>
       </Row>
       <Row>
-        <h5 className="sofia-pro mb-2 product-name">{itemName}</h5>
+        <h5 className='sofia-pro mb-2 product-name'>{itemName}</h5>
       </Row>
       <Row>
-        <h4 className="sofia-pro mb-0 product-price">
-          ${amount}
+        <h4 className='sofia-pro mb-0 product-price'>
+          ${amount}{' '}
+          {isHovering && (
+            <>
+              <span className='compensationText ml-2'>Cash back</span>
+              <span className='alternateActionText ml-2'>- Donate instead</span>
+            </>
+          )}
           <span
-            className="sofia-pro product-compensation-type"
+            className='sofia-pro product-compensation-type'
             style={{
               fontSize: 12,
               opacity: 0.6,
-              color: "#0e0018",
+              color: '#0e0018',
             }}
           >
             {compensationType}
