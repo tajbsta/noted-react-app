65b14d4054847065a9136173d96ff3b25c223fd1
Janica Dino <janicadino@gmail.com>
2021-04-24 02:46:42 +0800

    fix: Scheduled Return in profile page stylings

--- src/pages/Profile/components/ScheduledReturn.jsx
+++ src/pages/Profile/components/ScheduledReturn.jsx
@@ -4,6 +4,7 @@ import { Col, Row, Accordion, Card } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import ReturnScore from '../../../components/ReturnsScore';
+import ProductPlaceholder from '../../../assets/img/ProductPlaceholder.svg';
 import CancelOrderModal from './../../../modals/CancelOrderModal';
 
 export default function ScheduledReturn() {
@@ -22,40 +23,57 @@ export default function ScheduledReturn() {
     const thumbnail = get(item, 'thumbnail', '');
 
     return (
-      <Row className='mt-4'>
-        <Col xs={1}>
-          <img src={thumbnail} />
-        </Col>
-        <Col className='ml-4 mt-2'>
-          <Row>
-            <h4 className='mb-0 sofia-pro mb-1 distributor-name text-18'>
-              {vendorName}
-            </h4>
-          </Row>
-          <Row>
-            <h5 className='sofia-pro mb-2 product-name'>{name}</h5>
-          </Row>
-          <Row>
-            <h4 className='sofia-pro mb-0 product-price text-18'>${price}</h4>
-          </Row>
-        </Col>
-        <Col xs={1} className='mr-4'>
-          <Row>
-            <Col
-              className='ml-0'
-              xs={1}
-              style={{ display: 'flex', alignItems: 'center' }}
+      <div id='ScheduledReturnProduct'>
+        <Row className='mt-4'>
+          <Col className='sched-product-col'>
+            <div className='sched-img-col'>
+              <img
+                className='product-img'
+                style={{
+                  border: '1px solid #f0f0f0',
+                  borderRadius: '8px',
+                  justifyContent: 'center',
+                  width: '48px',
+                  height: '48px',
+                  background: '#fff',
+                }}
+                src={thumbnail || ProductPlaceholder}
+                onError={(e) => {
+                  e.currentTarget.src = ProductPlaceholder;
+                }}
+              />
+            </div>
+            <div style={{ marginLeft: '16px' }}>
+              <h4 className='sofia-pro sched-distributor-name'>{vendorName}</h4>
+              <h4 className='sofia-pro sched-product-name'>{name}</h4>
+              <h4 className='sofia-pro sched-product-price'>${price}</h4>
+            </div>
+          </Col>
+          <Col className='sched-vendor-col'>
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+              }}
             >
-              <ReturnScore score={rating} />
-            </Col>
-            <Col xs={1}>
+              <Col
+                className='ml-0'
+                style={{ display: 'flex', alignItems: 'center' }}
+              >
+                <ReturnScore score={rating} />
+              </Col>
               <div className='m-brand-logo-cont'>
-                <img src={thumbnail} alt='' className='m-brand-img' />
+                <img
+                  src='https://pbs.twimg.com/profile_images/1159166317032685568/hAlvIeYD_400x400.png'
+                  alt=''
+                  className='m-brand-img'
+                />
               </div>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+            </div>
+          </Col>
+        </Row>
+      </div>
     );
   };
 
@@ -66,7 +84,20 @@ export default function ScheduledReturn() {
     const renderAllScheduledItems = items.slice(0, 5).map((product) => {
       return (
         <li key={product}>
-          <img src={product.thumbnail} />
+          <img
+            style={{
+              border: '1px solid #f0f0f0',
+              borderRadius: '8px',
+              justifyContent: 'center',
+              width: '48px',
+              height: '48px',
+              background: '#fff',
+            }}
+            src={product.thumbnail || ProductPlaceholder}
+            onError={(e) => {
+              e.currentTarget.src = ProductPlaceholder;
+            }}
+          />
         </li>
       );
     });
@@ -95,25 +126,27 @@ export default function ScheduledReturn() {
               onCancel={onCancel}
             />
             {activeKey === '1' && (
-              <div className='card-body'>
-                <Row className='align-items-center'>
-                  <Col xs={2} className='title-col'>
-                    <Row>
-                      <div className='title'>Scheduled Return</div>
-                    </Row>
-                    <Row>
-                      <div className='total'>
-                        {get(items, 'length', 0)} items in total
-                      </div>
-                    </Row>
+              <div className='card-body initial-card-body'>
+                <Row
+                  className='align-items-center'
+                  style={{ marginLeft: '0px' }}
+                >
+                  <Col className='title-col'>
+                    <div className='title'>Scheduled Return</div>
+                    <div className='total'>
+                      {get(items, 'length', 0)} items in total
+                    </div>
                   </Col>
                   <Col className='product-img-col'>
                     {renderAllScheduledItems}
                   </Col>
-                  <Col xs={1} className='plus-more'>
-                    {items.length > 5 && `+${moreItemsCount} more...`}
-                  </Col>
-                  <Col xs={3} className='button-col'>
+                  {items.length > 5 && (
+                    <Col className='plus-more'>
+                      {items.length > 5 && `+${moreItemsCount} more...`}
+                    </Col>
+                  )}
+
+                  <Col className='button-col'>
                     <div>
                       <button
                         className='btn btn-show'
@@ -129,27 +162,21 @@ export default function ScheduledReturn() {
             )}
             <Accordion.Collapse eventKey='0'>
               <div>
-                <div className='p-4'>
-                  {/**
-                   * IS A CARD PRODUCT
-                   */}
+                <div style={{ padding: '24px', paddingBottom: '39px' }}>
                   <Row
                     className='align-items-center'
                     style={{
                       display: 'flex',
                       justifyContent: 'space-between',
+                      marginLeft: '0px',
                     }}
                   >
-                    <Col xs={2} className='title-col'>
-                      <Row>
-                        <div className='title'>Scheduled Return</div>
-                      </Row>
-                      <Row>
-                        <div className='total'>1 item total</div>
-                      </Row>
+                    <Col className='title-col'>
+                      <div className='title'>Scheduled Return</div>
+                      <div className='total'>1 item total</div>
                     </Col>
-                    <Col xs={2} className='title-col'>
-                      <div className='button-col mr-3'>
+                    <Col className='title-col'>
+                      <div className='button-col'>
                         <button
                           className='btn btn-show'
                           onClick={() => setActiveKey('1')}
@@ -163,30 +190,44 @@ export default function ScheduledReturn() {
 
                   {items.map((item) => renderScheduledReturnItem(item))}
                   <hr className='hr-line' />
-                  <Row className='action-container'>
-                    <Col xs={2}>
-                      <Row className='pick-up-duration-container'>
-                        <h5 className='sofia-pro mb-2 product-name'>
-                          Pick-up in
-                        </h5>
-                        <h4 className='sofia-pro product-price mt-1'>{`59 minutes`}</h4>
-                      </Row>
-                      <Row className='total-items-container'>
-                        <h5 className='sofia-pro mb-2 product-name'>Total</h5>
-                        <h4 className='sofia-pro product-price mt-1'>{`1 item`}</h4>
-                      </Row>
-                      <Row className='cancel-action-container'>
-                        <button
-                          className='btn btn-show p-0 m-0'
-                          onClick={() => {
-                            onCancel();
-                          }}
-                        >
-                          Modify or cancel return
-                        </button>
-                      </Row>
-                    </Col>
-                  </Row>
+
+                  <div className='sched-info-container'>
+                    <Row
+                      style={{ marginRight: '0px', justifyContent: 'flex-end' }}
+                    >
+                      <div className='sched-time-container'>
+                        <h4>Pick-up in &nbsp;</h4>
+                        <h4 className='sched-value'>{`59 minutes`}</h4>
+                      </div>
+                    </Row>
+                    <Row
+                      style={{
+                        justifyContent: 'flex-end',
+                        marginRight: '0px',
+                      }}
+                    >
+                      <div className='total-items-container'>
+                        <h4>Total&nbsp;</h4>
+                        <h4 className='sched-value'>{`1 item`}</h4>
+                      </div>
+                    </Row>
+
+                    <Row
+                      className='cancel-action-container'
+                      style={{
+                        marginRight: '0px',
+                      }}
+                    >
+                      <button
+                        className='btn btn-show p-0 m-0'
+                        onClick={() => {
+                          onCancel();
+                        }}
+                      >
+                        Modify or cancel return
+                      </button>
+                    </Row>
+                  </div>
                 </div>
               </div>
             </Accordion.Collapse>
