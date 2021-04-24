6d3134ceedb2a9797f95b05f4c91dbb56ec1a97d
Janica Dino <janicadino@gmail.com>
2021-04-24 16:50:40 +0800

    fix: ScheduledCard on dashboard

--- src/pages/Dashboard/components/ScheduledCard.jsx
+++ src/pages/Dashboard/components/ScheduledCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Col, Row } from 'react-bootstrap';
 import NoteeIcon from '../../../assets/icons/NoteeIcon.svg';
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import { get } from 'lodash-es';
 
 export default function ScheduledCard() {
+  const [isMobile, setIsMobile] = useState(false);
   const { scheduledReturns } = useSelector(
     ({ auth: { scheduledReturns } }) => ({
       scheduledReturns,
@@ -24,8 +25,19 @@ export default function ScheduledCard() {
     history.push('/profile');
   };
 
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 540);
+    }
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
   return (
-    <div id='ScheduledCard'>
+    <div className='col sched-col' id='ScheduledCard'>
       <div className='card'>
         <div className='card-body'>
           <Row style={{ alignItems: 'center' }}>
@@ -34,24 +46,56 @@ export default function ScheduledCard() {
                 <img src={NoteeIcon} />
               </div>
             </Col>
-            <Col xs={6}>
-              <Row>
-                <div className='title'>Your scheduled return</div>
-              </Row>
-              <Row>
-                <div className='items-info'>
-                  You have {get(allScheduledItems, 'length', 0)} items scheduled
-                  for return
-                </div>
-              </Row>
-            </Col>
-            <Col className='button-col'>
-              <div>
-                <button className='btn btn-view-scheduled' onClick={profile}>
-                  View scheduled returns
-                </button>
-              </div>
-            </Col>
+            {!isMobile && (
+              <>
+                <Col xs={6} className='info-col'>
+                  <Row>
+                    <div className='title'>Your scheduled return</div>
+                  </Row>
+                  <Row>
+                    <div className='items-info'>
+                      You have {get(allScheduledItems, 'length', 0)} items
+                      scheduled for return
+                    </div>
+                  </Row>
+                </Col>
+                <Col className='button-col'>
+                  <div>
+                    <button
+                      className='btn btn-view-scheduled'
+                      onClick={profile}
+                    >
+                      View scheduled returns
+                    </button>
+                  </div>
+                </Col>
+              </>
+            )}
+            {/* START OF MOBILE VIEWS */}
+            {isMobile && (
+              <>
+                <Col className='info-col ml-5'>
+                  <Row>
+                    <div className='title'>Your scheduled return</div>
+                  </Row>
+                  <Row>
+                    <div className='items-info'>
+                      You have {get(allScheduledItems, 'length', 0)} items
+                      scheduled for return
+                    </div>
+                  </Row>
+                  <Row>
+                    <button
+                      className='btn btn-view-scheduled'
+                      onClick={profile}
+                    >
+                      View scheduled returns
+                    </button>
+                  </Row>
+                </Col>
+              </>
+            )}
+            {/* END OF MOBILE VIEWS*/}
           </Row>
         </div>
       </div>
