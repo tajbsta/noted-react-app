c5e0d8eceb9c0d0edbf651f270a44ad6f39e2385
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-05-03 20:21:14 +0800

    fix: fixed days left condition

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -190,7 +190,7 @@ function ProductCard({
                 }}
               >
                 <NotedCheckbox
-                  disabled={disabled || daysLeft > 2}
+                  disabled={disabled || daysLeft <= 2}
                   checked={selected}
                   onChangeState={handleSelection}
                 />
