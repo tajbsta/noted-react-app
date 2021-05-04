6bdda94a04c7383c347d7bf8aaa2338332d2aa1f
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-05-04 22:30:20 +0800

    feat: inital product in review

--- src/pages/Profile/components/ProductInReviewCard.jsx
+++ src/pages/Profile/components/ProductInReviewCard.jsx
@@ -2,6 +2,7 @@ import ProductPlaceholder from '../../../assets/img/ProductPlaceholder.svg';
 import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { get } from 'lodash-es';
+import ReturnScore from '../../../components/ReturnsScore';
 
 export default function ProductInReviewCard({ item }) {
   const [isMobile, setIsMobile] = useState(false);
@@ -50,6 +51,20 @@ export default function ProductInReviewCard({ item }) {
   };
 
   const formattedProductName = toTitleCase(item.itemName);
+  const formatPrice = item.amount.toFixed(2);
+
+  const RenderStatus = (status) => {
+    switch (status) {
+      case 'pending':
+        return <h4 className='sofia-pro pending-status mb-0'>Pending</h4>;
+      case 'rejected':
+        return <h4 className='sofia-pro rejected-status mb-0'>Rejected</h4>;
+      case 'accepted':
+        return <h4 className='sofia-pro accepted-status mb-0'>Accepted</h4>;
+      default:
+        return <></>;
+    }
+  };
 
   return (
     <div id='productCard'>
@@ -59,12 +74,7 @@ export default function ProductInReviewCard({ item }) {
           style={{ marginTop: isMobile ? '1px' : '' }}
         >
           <Row>
-            <div
-              className='product-img-container'
-              style={{
-                display: 'flex',
-              }}
-            >
+            <div className='product-img-container'>
               <img
                 className='product-img ml-3'
                 src={item.thumbnail || ProductPlaceholder}
@@ -79,7 +89,7 @@ export default function ProductInReviewCard({ item }) {
                 }}
               />
             </div>
-            <Col>
+            <Col xs={3}>
               <Container>
                 <div className='title-container'>
                   <h4
@@ -88,18 +98,81 @@ export default function ProductInReviewCard({ item }) {
                   >
                     {get(item, 'vendorTag', 'Product Name')}
                   </h4>
-                  {isMobileSmaller && (
-                    <h4
-                      className='sofia-pro mb-2 product-name'
-                      style={{ lineHeight: 'inherit' }}
-                    >
-                      {truncateProductNameForSmallerScreens(
-                        formattedProductName
-                      )}
-                    </h4>
-                  )}
+                  <h4
+                    className='sofia-pro mb-2 product-name'
+                    style={{ lineHeight: 'inherit' }}
+                  >
+                    {formattedProductName}
+                  </h4>
+                  <h4 className='sofia-pro product-price'>${formatPrice}</h4>
                 </div>
               </Container>
+            </Col>
+            <Col
+              xs={7}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                paddingRight: '0px',
+                marginLeft: '2em',
+                justifyContent: 'flex-end',
+              }}
+            >
+              <Row
+                xs={5}
+                style={{
+                  minWidth: '50%',
+                }}
+              >
+                <Col
+                  xs={9}
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    alignItems: 'center',
+                    paddingRight: '0px',
+                  }}
+                >
+                  <Row
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <Col
+                      style={{
+                        display: 'flex',
+                        alignContent: 'center',
+                      }}
+                    >
+                      {RenderStatus(get(item, 'status', 'pending'))}
+                    </Col>
+                    <Col>
+                      <ReturnScore score={get(item, 'vendor_data.rating', 1)} />
+                    </Col>
+                  </Row>
+                </Col>
+                <Col
+                  style={{
+                    marginLeft: 5,
+                  }}
+                >
+                  <img
+                    src={get(item, 'vendor_data.thumbnail', ProductPlaceholder)}
+                    onError={(e) => {
+                      e.currentTarget.src = ProductPlaceholder;
+                    }}
+                    alt=''
+                    className='avatar-img rounded-circle noted-border brand-img'
+                    style={{
+                      width: 35,
+                      height: 35,
+                      objectFit: 'contain',
+                      background: '#fff',
+                    }}
+                  />
+                </Col>
+              </Row>
             </Col>
           </Row>
         </div>
