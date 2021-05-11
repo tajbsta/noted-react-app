28c0ad27aae66658e2355d73c5c8f9976fa341d6
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-05-11 13:43:18 +0800

    fix: enables x mark in checkout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -207,7 +207,7 @@ function ProductCard({
                 marginTop: isMobile && selected ? '7px' : '',
               }}
             >
-              {removable && !selectable && !confirmed && isViewScan !== false && (
+              {removable && !selectable && !confirmed && (
                 <div
                   className='removeProduct'
                   onClick={() => {
