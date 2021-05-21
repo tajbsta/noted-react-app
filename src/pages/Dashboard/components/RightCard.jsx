6e8ba64e803d2628152834d2f67275950bdd6914
Janica Dino <janicadino@gmail.com>
2021-05-22 03:30:31 +0800

    connect pickup price & refactor pickup button

--- src/pages/Dashboard/components/RightCard.jsx
+++ src/pages/Dashboard/components/RightCard.jsx
@@ -1,88 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { Col, OverlayTrigger, Spinner, Tooltip } from 'react-bootstrap'
-import { useHistory } from 'react-router'
-import { useDispatch, useSelector } from 'react-redux'
-import { isEmpty } from 'lodash'
-import axios from 'axios'
-
-import HorizontalLine from '../../../components/HorizontalLine'
-import Row from '../../../components/Row'
-import PickUpButton from './PickUpButton'
-import { calculateMetrics } from '../../../utils/productsApi'
-import NotedCheckbox from '../../../components/NotedCheckbox'
-import { setCartItems } from '../../../actions/cart.action'
+import React, { useState, useEffect } from 'react';
+import { Col, OverlayTrigger, Spinner, Tooltip } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { isEmpty } from 'lodash';
+import axios from 'axios';
+import HorizontalLine from '../../../components/HorizontalLine';
+import Row from '../../../components/Row';
+import PickUpButton from './PickUpButton';
+import { calculateMetrics } from '../../../utils/productsApi';
+import NotedCheckbox from '../../../components/NotedCheckbox';
+import { setCartItems } from '../../../actions/cart.action';
 import {
   DONATE,
   LAST_CALL,
   NOT_ELIGIBLE,
   RETURNABLE,
-} from '../../../constants/actions/runtime'
-// import infoIcon from '../../../assets/icons/info.svg'
-import InfoIcon from '../../../components/InfoIcon'
+} from '../../../constants/actions/runtime';
+import InfoIcon from '../../../components/InfoIcon';
 
 function RightCard({ userId, setSelectedProducts }) {
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const [isMobile, setIsMobile] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [isMobile, setIsMobile] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { items } = useSelector(({ cart: { items } }) => ({
     items,
-  }))
+  }));
 
   const [pricing, setPricing] = useState({
     totalReturns: 0,
     potentialReturnValue: 0,
     totalDonations: 0,
     pickupPrice: 0,
-  })
+  });
 
   // Check if device is mobile
   useEffect(() => {
     function handleResize() {
-      setIsMobile(window.innerWidth <= 767)
+      setIsMobile(window.innerWidth <= 767);
     }
-    handleResize() // Run on load to set the default value
-    window.addEventListener('resize', handleResize)
+    handleResize(); // Run on load to set the default value
+    window.addEventListener('resize', handleResize);
     // Clean up event listener
     return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  })
+      window.removeEventListener('resize', handleResize);
+    };
+  });
 
   useEffect(() => {
     // if (!isEmpty(xorWith(items, previousCartItems, isEqual))) {
     //   calculateCurrentCartPricing(items);
     // }
-    calculateCurrentCartPricing(items)
-  }, [items])
+    calculateCurrentCartPricing(items);
+  }, [items]);
 
   const calculateCurrentCartPricing = async (currentItems) => {
     try {
       // if (userId) {
-      setLoading(true)
+      setLoading(true);
 
-      const cartItems = [...currentItems]
-      const productIds = cartItems.map((x) => x._id)
+      const cartItems = [...currentItems];
+      const productIds = cartItems.map((x) => x._id);
 
-      const data = await calculateMetrics(userId, productIds)
+      const data = await calculateMetrics(userId, productIds);
 
       setPricing({
         totalReturns: data.totalReturns,
         potentialReturnValue: data.potentialReturnValue,
         totalDonations: data.totalDonations,
         pickupPrice: data.pickupPrice,
-      })
-      setLoading(false)
+      });
+      setLoading(false);
       // }
     } catch (error) {
       if (!axios.isCancel(error)) {
-        setLoading(false)
+        setLoading(false);
         /**
          * A toast error should be added here
          */
       }
     }
-  }
+  };
 
   const handleCheckboxChange = () => {
     setSelectedProducts({
@@ -90,14 +88,14 @@ function RightCard({ userId, setSelectedProducts }) {
       [NOT_ELIGIBLE]: [],
       [RETURNABLE]: [],
       [DONATE]: [],
-    })
-    dispatch(setCartItems([]))
-  }
+    });
+    dispatch(setCartItems([]));
+  };
 
   return (
     <div style={{ position: !isMobile ? 'fixed' : '' }}>
       <div
-        className="col right-card mt-4"
+        className='col right-card mt-4'
         style={{
           minWidth: '248px',
         }}
@@ -111,12 +109,12 @@ function RightCard({ userId, setSelectedProducts }) {
           id={loading && !isMobile ? 'overlay' : ''}
         >
           {!isMobile && (
-            <div className="p-0 ml-1 d-inline-flex align-center">
-              <h5 className="card-title mb-0 p-3 sofia-pro">
+            <div className='p-0 ml-1 d-inline-flex align-center'>
+              <h5 className='card-title mb-0 p-3 sofia-pro'>
                 {isEmpty(items) && <div>Total past 90 days</div>}
 
                 {!isEmpty(items) && (
-                  <div className="row">
+                  <div className='row'>
                     <Col xs={1}>
                       <NotedCheckbox
                         disabled={loading}
@@ -134,24 +132,24 @@ function RightCard({ userId, setSelectedProducts }) {
             </div>
           )}
 
-          {!isMobile && <HorizontalLine width="90%" />}
-          <div className="card-body p-0">
+          {!isMobile && <HorizontalLine width='90%' />}
+          <div className='card-body p-0'>
             <div className={`${isMobile ? 'mobile-padding pb-1' : 'p-2'}`}>
               {loading && (
-                <div className="d-flex justify-content-center mt-2 r-spin-container">
-                  <Spinner animation="border" size="md" className="spinner" />
+                <div className='d-flex justify-content-center mt-2 r-spin-container'>
+                  <Spinner animation='border' size='md' className='spinner' />
                 </div>
               )}
 
-              <div className="mobile-container">
+              <div className='mobile-container'>
                 {!isMobile && (
                   <>
                     <Row marginTop={3} marginLeft={2}>
-                      <div className="col-7 total-returns">
-                        <div className="row card-text mb-0 sofia-pro card-value">
+                      <div className='col-7 total-returns'>
+                        <div className='row card-text mb-0 sofia-pro card-value'>
                           {pricing.totalReturns}
                         </div>
-                        <div className="row card-text card-label">
+                        <div className='row card-text card-label'>
                           Total Returns
                         </div>
                       </div>
@@ -159,14 +157,14 @@ function RightCard({ userId, setSelectedProducts }) {
                     <Row
                       marginTop={3}
                       marginLeft={2}
-                      className="p-0 return-value-container"
+                      className='p-0 return-value-container'
                     >
-                      <div className="col-5">
-                        <div className="row card-text mb-0 sofia-pro card-value">
+                      <div className='col-5'>
+                        <div className='row card-text mb-0 sofia-pro card-value'>
                           ${Number(pricing.potentialReturnValue).toFixed(2)}
                         </div>
-                        <div className="row small sofia-pro card-label text-potential-value d-flex ">
-                          <span className="my-auto mr-2">
+                        <div className='row small sofia-pro card-label text-potential-value d-flex '>
+                          <span className='my-auto mr-2'>
                             Potential Return Value
                           </span>
                           <InfoIcon />
@@ -175,13 +173,13 @@ function RightCard({ userId, setSelectedProducts }) {
                     </Row>
 
                     <hr />
-                    <Row marginTop={3} marginLeft={2} className="p-0">
-                      <div className="col-12 p-0">
-                        <div className="col-sm-8">
-                          <div className="row mb-0 sofia-pro card-value">
+                    <Row marginTop={3} marginLeft={2} className='p-0'>
+                      <div className='col-12 p-0'>
+                        <div className='col-sm-8'>
+                          <div className='row mb-0 sofia-pro card-value'>
                             {pricing.totalDonations}
                           </div>
-                          <div className="row card-text small sofia-pro card-label total-donations">
+                          <div className='row card-text small sofia-pro card-label total-donations'>
                             Total Donations
                           </div>
                         </div>
@@ -193,12 +191,12 @@ function RightCard({ userId, setSelectedProducts }) {
                 {/* START OF MOBILE VIEWS */}
                 {isMobile && (
                   <>
-                    <div className="p-0 ml-1 d-inline-flex align-center">
-                      <h5 className="card-title sofia-pro m-card-title">
+                    <div className='p-0 ml-1 d-inline-flex align-center'>
+                      <h5 className='card-title sofia-pro m-card-title'>
                         {isEmpty(items) && <div>Total past 90 days</div>}
 
                         {!isEmpty(items) && (
-                          <div className="row">
+                          <div className='row'>
                             <Col xs={1}>
                               <NotedCheckbox
                                 disabled={loading}
@@ -215,33 +213,33 @@ function RightCard({ userId, setSelectedProducts }) {
                         )}
                       </h5>
                     </div>
-                    <Row className="m-right-card-row">
-                      <Col className="m-right-card-col">
+                    <Row className='m-right-card-row'>
+                      <Col className='m-right-card-col'>
                         <Row>
-                          <div className="m-right-card-val">
+                          <div className='m-right-card-val'>
                             <h4>
                               ${Number(pricing.potentialReturnValue).toFixed(2)}
                             </h4>
                           </div>
                         </Row>
                         <Row>
-                          <div className="m-label d-flex">
-                            <h4 className="mr-2">Potential Return Value</h4>
+                          <div className='m-label d-flex'>
+                            <h4 className='mr-2'>Potential Return Value</h4>
                             <InfoIcon
-                              className="info-icon"
+                              className='info-icon'
                               isMobile={isMobile}
                             />
                           </div>
                         </Row>
                       </Col>
-                      <Col className="m-right-card-col">
+                      <Col className='m-right-card-col'>
                         <Row>
-                          <div className="m-right-card-val">
+                          <div className='m-right-card-val'>
                             <h4>{pricing.totalDonations}</h4>
                           </div>
                         </Row>
                         <Row>
-                          <div className="m-label">
+                          <div className='m-label'>
                             <h4>Total Donations</h4>
                           </div>
                         </Row>
@@ -253,7 +251,7 @@ function RightCard({ userId, setSelectedProducts }) {
               </div>
 
               <div
-                className="pr-3 pl-3 mt-3 pickup-value"
+                className='pr-3 pl-3 mt-3 pickup-value'
                 style={{
                   opacity: !items.length ? 0.37 : 1,
                   paddingLeft: isMobile ? '16px' : '',
@@ -261,13 +259,13 @@ function RightCard({ userId, setSelectedProducts }) {
                 }}
               >
                 <PickUpButton
-                  leadingText="Pickup"
+                  leadingText='Pickup'
                   disabled={!items.length || loading}
-                  price="9.99"
-                  backgroundColor="#570097"
-                  textColor="white"
+                  price={pricing.pickupPrice}
+                  backgroundColor='#570097'
+                  textColor='white'
                   onClick={() => {
-                    history.push('/checkout')
+                    history.push('/checkout');
                   }}
                 />
               </div>
@@ -276,7 +274,7 @@ function RightCard({ userId, setSelectedProducts }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default RightCard
+export default RightCard;
