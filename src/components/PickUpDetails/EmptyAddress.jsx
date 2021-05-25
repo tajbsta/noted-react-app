b2de3075e87ed5137a3e21a06c283da7a7fec135
Janica Dino <janicadino@gmail.com>
2021-05-25 22:29:31 +0800

    add loader in empty forms to hide delay

--- src/components/PickUpDetails/EmptyAddress.jsx
+++ src/components/PickUpDetails/EmptyAddress.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import { timeout } from '../../utils/time';
 import $ from 'jquery';
 
 export default function EmptyAddress(props) {
   const [isMobile, setIsMobile] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const platform = window.navigator.platform;
@@ -25,32 +28,69 @@ export default function EmptyAddress(props) {
     };
   });
 
+  async function renderSpinner() {
+    setLoading(true);
+    await timeout(300);
+  }
+
+  const renderStopSpinner = () => {
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    renderSpinner();
+    setTimeout(() => {
+      renderStopSpinner();
+    }, 1000);
+  }, []);
+
   return (
     <div>
       <div
         id='EmptyState'
         className='card-body payment-details-card-body pb-3 pl-4 m-0'
       >
-        <div
-          className='justify-content-center'
-          style={{ display: isMobile ? 'none' : 'flex' }}
-        >
-          <div className='p-0'>
-            <p className='empty-header sofia-pro text-14 line-height-16 margin-bottom'>
-              Pick-up Address
-            </p>
+        {loading && (
+          <div
+            className={`d-flex justify-content-center ${
+              !isMobile ? 'mt-5' : ''
+            }`}
+          >
+            <Spinner
+              animation='border'
+              size='md'
+              style={{
+                color: '#570097',
+                opacity: '0.6',
+              }}
+              className='spinner'
+            />
           </div>
-        </div>
-        <div>
-          <h4 className='p-0 m-0 sofia-pro text-center empty-sub'>
-            Adding a pick-up address is necessary to proceed.
-          </h4>
-        </div>
-        <div className='d-flex justify-content-center'>
-          <button className='btn btn-add-empty' onClick={props.onClick}>
-            Add Pick-up Address
-          </button>
-        </div>
+        )}
+        {!loading && (
+          <>
+            <div
+              className='justify-content-center'
+              style={{ display: isMobile ? 'none' : 'flex' }}
+            >
+              <div className='p-0'>
+                <p className='empty-header sofia-pro text-14 line-height-16 margin-bottom'>
+                  Pick-up Address
+                </p>
+              </div>
+            </div>
+            <div>
+              <h4 className='p-0 m-0 sofia-pro text-center empty-sub'>
+                Adding a pick-up address is necessary to proceed.
+              </h4>
+            </div>
+            <div className='d-flex justify-content-center'>
+              <button className='btn btn-add-empty' onClick={props.onClick}>
+                Add Pick-up Address
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
