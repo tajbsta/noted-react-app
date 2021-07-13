0423d0ce1af7cf6e5aa97d32a1a85e30eecea632
Adewusi Victor <glove.adewusi@gmail.com>
2021-07-13 14:51:44 +0100

    Setup Initial Client Scraper

---
+++ src/library/scraper.library.js
@@ -0,0 +1,133 @@
+import * as _ from 'lodash/array';
+import base64url from 'base64url';
+/**
+ *
+ * @param {string} email
+ * @returns boolean
+ */
+const validateEmail = (email = '') => {
+  /* eslint-disable no-useless-escape */
+  const tester = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+  return tester.test(email);
+};
+
+/**
+ * GET VENDORS FROM EMAILS
+ * @param {Array} vendors
+ * @returns {Array} emails - From Emails for vendors
+ */
+export const getVendorsFromEmail = (vendors) => {
+  const emails = [];
+  vendors.forEach((vendor) => {
+    const fromEmails = (vendor.from_emails || '').split(',');
+    fromEmails.forEach((x) => {
+      const email = x.trim();
+      if (validateEmail(email)) {
+        emails.push(email);
+      }
+    });
+  });
+
+  return emails;
+};
+
+/**BUILD EMAIL QUERY
+ * @param {Object} initialQuery
+ * @param {Array<string>} initialQuery.from
+ * @param {string} initialQuery.before
+ * @param {string} initialQuery.after
+ * @returns queryString<string>
+ *
+ */
+export const buildEmailQuery = (initialQuery) => {
+  const query = [];
+  if (initialQuery.from) {
+    initialQuery.from.forEach((from) => {
+      query.push([`from:${from}`]);
+    });
+  }
+  const queryString = query
+    .map((conditions) =>
+      conditions.length > 1
+        ? `(${conditions.join(' AND ')})`
+        : `${conditions[0]}`
+    )
+    .join(' OR ');
+
+  const before = initialQuery.before;
+  const after = initialQuery.after;
+
+  return `before:${before} after:${after} (${queryString})`;
+};
+
+/**
+ * FETCH ACCOUNT EMAILS
+ * @param {string} q
+ * @param {Object} gapi - Google API attached to window object
+ * @returns {Promise<Array<string>>} emails
+ */
+export const getAccountMessages = async (q, gapi) => {
+  let messages = [];
+  let nextPageToken = '';
+
+  while (nextPageToken !== undefined) {
+    const response = await gapi.current.client.gmail.users.messages.list({
+      userId: 'me',
+      maxResults: 500,
+      q,
+      pageToken: nextPageToken,
+    });
+
+    messages =
+      response.result.resultSizeEstimate > 0
+        ? [...messages, ...response.result.messages]
+        : messages;
+    nextPageToken = response.result.nextPageToken || undefined;
+  }
+
+  const messageIds = messages.map((x) => x.id);
+
+  const emails = await convertMessagesToEmails(messageIds, gapi);
+  return emails;
+};
+
+/**
+ * CONVERT MESSAGES TO EMAILS
+ * @param {Array<string>} messageIds
+ * @param {Object} gapi - Google API attached to window object
+ */
+export const convertMessagesToEmails = async (messageIds, gapi) => {
+  const chunkMessages = _.chunk(messageIds, 100);
+
+  const batchResponses = await Promise.all(
+    chunkMessages.map(async (ids) => {
+      const batch = gapi.current.client.newBatch();
+      ids.forEach((id) => {
+        const getEmail = gapi.current.client.gmail.users.messages.get({
+          userId: 'me',
+          id,
+          format: 'raw',
+        });
+        batch.add(getEmail);
+      });
+      const res = await batch;
+      return res;
+    })
+  );
+
+  const emails = [];
+  for (const batchRes of batchResponses) {
+    Object.values(batchRes.result).forEach((res) => {
+      const rawEmail = {
+        raw: base64url.toBase64(res.result.raw),
+        internalDate: res.result.internalDate,
+        // id: res.result.id,
+        // sizeEstimate: res.result.sizeEstimate,
+      };
+      emails.push(rawEmail);
+    });
+  }
+
+  return emails;
+};
--- src/pages/Dashboard/DashboardPageInitial.jsx
+++ src/pages/Dashboard/DashboardPageInitial.jsx
@@ -4,145 +4,143 @@ import AuthorizeImg from '../../assets/img/Authorize.svg';
 import ScanningIcon from '../../assets/icons/Scanning.svg';
 import CustomRow from '../../components/Row';
 import { loadGoogleScript } from '../../library/loadGoogleScript';
+import { getVendors } from '../../api/productsApi';
+import {
+  getVendorsFromEmail,
+  buildEmailQuery,
+  getAccountMessages,
+} from '../../library/scraper.library';
+import moment from 'moment';
+import { useRef } from 'react';
+import * as _ from 'lodash/array';
+import axios from 'axios';
+import base64url from 'base64url';
 
 const Authorize = ({ triggerScanNow }) => {
-    return (
-        <div id="AuthorizeUpdate">
-            <Container className="main-body" fluid="lg">
-                <Row md="2" className="text-left align-items-end">
-                    <Col xs="6" className="info">
-                        <h1 className="bold text-title">
-                            Everything is automatic
-                        </h1>
-                        <h4 className="text-noted">
-                            noted will scan your email inbox and find all of
-                            your online purchases and their return limits.
-                        </h4>
-                        <div className="text-subtitle">
-                            <h4 className="bold">In time?</h4>
-                            <h4>Get your cash back with one click</h4>
-                        </div>
-                        <div className="text-subtitle">
-                            <h4 className="bold">Too late?</h4>
-                            <h4>
-                                Declutter your home and donate to local
-                                charities
-                            </h4>
-                        </div>
+  return (
+    <div id="AuthorizeUpdate">
+      <Container className="main-body" fluid="lg">
+        <Row md="2" className="text-left align-items-end">
+          <Col xs="6" className="info">
+            <h1 className="bold text-title">Everything is automatic</h1>
+            <h4 className="text-noted">
+              noted will scan your email inbox and find all of your online
+              purchases and their return limits.
+            </h4>
+            <div className="text-subtitle">
+              <h4 className="bold">In time?</h4>
+              <h4>Get your cash back with one click</h4>
+            </div>
+            <div className="text-subtitle">
+              <h4 className="bold">Too late?</h4>
+              <h4>Declutter your home and donate to local charities</h4>
+            </div>
 
-                        <h4 className="text-first">
-                            You first need to authorized noted to read your
-                            emails. Only bots will see the relevant emails and
-                            we will never sell or transfer any of your personal
-                            info to anyone.
-                        </h4>
-                        <h4 className="text-underline">
-                            <a
-                                href="https://notedreturns.com/privacy-policy"
-                                target="_blank"
-                                rel="noreferrer"
-                                className="sofia-pro"
-                            >
-                                Learn more about security
-                            </a>
-                        </h4>
-                        <Button
-                            onClick={triggerScanNow}
-                            className="btn btn-green btn-authorize"
-                        >
-                            Scan Now
-                        </Button>
-                    </Col>
-                    <Col xs="6">
-                        <div className="authorize-img">
-                            <img src={AuthorizeImg} />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            {/* MOBILE VIEW */}
-            <Container
-                className="main-body-mobile"
-                fluid="lg"
-                style={{ marginTop: '2.5rem' }}
+            <h4 className="text-first">
+              You first need to authorized noted to read your emails. Only bots
+              will see the relevant emails and we will never sell or transfer
+              any of your personal info to anyone.
+            </h4>
+            <h4 className="text-underline">
+              <a
+                href="https://notedreturns.com/privacy-policy"
+                target="_blank"
+                rel="noreferrer"
+                className="sofia-pro"
+              >
+                Learn more about security
+              </a>
+            </h4>
+            <Button
+              onClick={triggerScanNow}
+              className="btn btn-green btn-authorize"
             >
-                <Row md="2" className="text-left align-items-end">
-                    <Col xs="6">
-                        <div className="authorize-img-mobile">
-                            <img src={AuthorizeImg} />
-                        </div>
-                    </Col>
-                    <Col xs="6" className="info">
-                        <h1 className="bold text-title">
-                            Everything is automatic
-                        </h1>
-                        <h4 className="text-noted">
-                            noted will scan your email inbox and find all of
-                            your online purchases and their return limits.
-                        </h4>
-                        <div className="text-subtitle">
-                            <h4 className="bold">In time?</h4>
-                            <h4 className="subtitle">
-                                Get your cash back with one click
-                            </h4>
-                        </div>
-                        <div className="text-subtitle">
-                            <h4 className="bold">Too late?</h4>
-                            <h4 className="subtitle">
-                                Declutter your home and donate to local
-                                charities
-                            </h4>
-                        </div>
+              Scan Now
+            </Button>
+          </Col>
+          <Col xs="6">
+            <div className="authorize-img">
+              <img src={AuthorizeImg} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      {/* MOBILE VIEW */}
+      <Container
+        className="main-body-mobile"
+        fluid="lg"
+        style={{ marginTop: '2.5rem' }}
+      >
+        <Row md="2" className="text-left align-items-end">
+          <Col xs="6">
+            <div className="authorize-img-mobile">
+              <img src={AuthorizeImg} />
+            </div>
+          </Col>
+          <Col xs="6" className="info">
+            <h1 className="bold text-title">Everything is automatic</h1>
+            <h4 className="text-noted">
+              noted will scan your email inbox and find all of your online
+              purchases and their return limits.
+            </h4>
+            <div className="text-subtitle">
+              <h4 className="bold">In time?</h4>
+              <h4 className="subtitle">Get your cash back with one click</h4>
+            </div>
+            <div className="text-subtitle">
+              <h4 className="bold">Too late?</h4>
+              <h4 className="subtitle">
+                Declutter your home and donate to local charities
+              </h4>
+            </div>
 
-                        <h4 className="text-first">
-                            You first need to authorized noted to read your
-                            emails. Only bots will see the relevant emails and
-                            we will never sell or transfer any of your personal
-                            info to anyone.
-                        </h4>
-                        <h4 className="text-underline">
-                            <a href="#" className="sofia-pro">
-                                Learn more about security
-                            </a>
-                        </h4>
-                        <Button
-                            onClick={triggerScanNow}
-                            className="btn btn-green btn-authorize"
-                        >
-                            Scan Now
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+            <h4 className="text-first">
+              You first need to authorized noted to read your emails. Only bots
+              will see the relevant emails and we will never sell or transfer
+              any of your personal info to anyone.
+            </h4>
+            <h4 className="text-underline">
+              <a href="#" className="sofia-pro">
+                Learn more about security
+              </a>
+            </h4>
+            <Button
+              onClick={triggerScanNow}
+              className="btn btn-green btn-authorize"
+            >
+              Scan Now
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 const Scanning = () => {
-    return (
-        <div id="ScanningUpdate">
-            <div className="card-body">
-                <CustomRow marginBottom={2}>
-                    <div
-                        className="col-12"
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <img src={ScanningIcon} />
-                    </div>
-                </CustomRow>
-                <p className="text-center sofia-pro noted-purple text-18 text-subtitle">
-                    Scan running...
-                </p>
-                <p className="small text-muted mb-1 text-center text-16 sofia-pro">
-                    Go have some coffee - we&apos;ll email ya when it&apos;s
-                    done!
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div id="ScanningUpdate">
+      <div className="card-body">
+        <CustomRow marginBottom={2}>
+          <div
+            className="col-12"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <img src={ScanningIcon} />
+          </div>
+        </CustomRow>
+        <p className="text-center sofia-pro noted-purple text-18 text-subtitle">
+          Scan running...
+        </p>
+        <p className="small text-muted mb-1 text-center text-16 sofia-pro">
+          Go have some coffee - we&apos;ll email ya when it&apos;s done!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 /**STATUSES
@@ -153,150 +151,118 @@ const Scanning = () => {
  *  */
 
 const DashboardPageInitial = () => {
-    const [status, setStatus] = useState('');
+  const [status, setStatus] = useState('');
+  const gapi = useRef(null);
+  const allVendors = useRef(null);
 
-    /**TRIGGER SCAN NOW FOR USERS */
-    const triggerScanNow = () => {
-        window.gapi.auth2.getAuthInstance().signIn();
-        setStatus('isAuthorizing');
+  /**TRIGGER SCAN NOW FOR USERS */
+  const triggerScanNow = () => {
+    gapi.current.auth2.getAuthInstance().signIn();
+    setStatus('isAuthorizing');
+  };
 
-        // setTimeout(() => {
-        //     setStatus('isScraping');
-        // }, 3000);
-    };
+  //FETCH VENDORS
+  const fetchVendors = async () => {
+    const data = await getVendors();
+    allVendors.current = data;
+    setStatus('notAuthorized');
+  };
 
-    const fetchByVendor = async (from, initialToken) => {
-        let emails = [];
-        let nextPageToken = initialToken;
+  /**HANDLE EMAIl SCRAPING */
+  const handleScraping = async () => {
+    try {
+      setStatus('isScraping');
+      const vendors = allVendors.current;
 
-        while (nextPageToken !== undefined) {
-            const response = await window.gapi.client.gmail.users.messages.list(
-                {
-                    userId: 'me',
-                    maxResults: 10,
-                    q: from,
-                    pageToken: nextPageToken,
-                }
-            );
+      const before = moment().format('YYYY/MM/DD');
+      const after = moment().subtract(90, 'days').format('YYYY/MM/DD');
 
-            emails =
-                response.result.resultSizeEstimate > 0
-                    ? [...emails, ...response.result.messages]
-                    : emails;
-            nextPageToken = response.result.nextPageToken || undefined;
+      const q = {
+        // from: getVendorsFromEmail([{ from_emails: 'gabriella@deel.support' }]),
+        from: getVendorsFromEmail(vendors),
+        after,
+        before,
+      };
+
+      //BUILD EMAIL QUERY
+      const emailQuery = buildEmailQuery(q);
+      const emails = await getAccountMessages(emailQuery, gapi);
+
+      if (emails.length <= 0) {
+        //HANDLE NO EMAIL AVAILABLE FOR SCRAPING
+        throw Error('No Email Available for Scraping');
+      }
+
+      //CURRENTLY USING DATA FROM S3 TO TEST
+      //TODO- E2E testing with noted@notedreturns.com
+      const response = await axios.get('/data.json');
+      const nord = await response.data;
+      nord.raw = base64url.toBase64(nord.raw);
+
+      const data = await window.notedScraper(vendors, [nord]);
+
+      //SEND TO BACKEND
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  /**INITIALIZE CLIENT TO USE GAPI */
+  const initClient = async () => {
+    try {
+      await gapi.current.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        discoveryDocs: [
+          'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest',
+        ],
+        scope: 'https://www.googleapis.com/auth/gmail.readonly',
+      });
+
+      const isSignedIn = gapi.current.auth2.getAuthInstance().isSignedIn.get();
+
+      if (isSignedIn) {
+        gapi.current.auth2.getAuthInstance().signOut();
+      }
+
+      // Listen for sign-in state changes.
+      gapi.current.auth2.getAuthInstance().isSignedIn.listen((success) => {
+        if (success) {
+          handleScraping();
         }
-        return emails;
+      });
+    } catch (error) {
+      console.log(JSON.stringify(error, null, 2));
+    }
+  };
+
+  useEffect(() => {
+    fetchVendors();
+  }, []);
+
+  //INITIALIZE GOOGLE API
+  useEffect(() => {
+    window.onGoogleScriptLoad = () => {
+      gapi.current = window.gapi;
+      gapi.current.load('client:auth2', initClient);
     };
 
-    //FETCH EMAILS
-    const fetchEmails = async () => {
-        const vendorList = [
-            'from:gabriella@deel.support',
-            'from:support@udacity.com',
-            'no_reply@monday.com',
-        ];
-        const emailContainer = await Promise.all(
-            vendorList.map(async (vendorFrom) => {
-                const em = await fetchByVendor(vendorFrom, '');
-                return em;
-            })
-        );
-        const emails = emailContainer.reduce((prev, curr) => {
-            return [...prev, ...curr];
-        }, []);
+    loadGoogleScript();
+  }, []);
 
-        const batch = window.gapi.client.newBatch();
-
-        emails.forEach((email) => {
-            const getEmail = window.gapi.client.gmail.users.messages.get({
-                userId: 'me',
-                id: email.id,
-                q: 'format: raw',
-            });
-            batch.add(getEmail);
-        });
-
-        const res = await batch;
-        const scrapedEmails = Object.values(res.result).map((item) => {
-            const internalDate = item.result.internalDate;
-            const value = item.result.payload.parts.filter(
-                (item) => item.mimeType === 'text/html'
-            )[0].body.data;
-            return {
-                raw: value,
-                internalDate,
-            };
-        });
-        return scrapedEmails;
-    };
-
-    /**HANDLE EMAIl SCRAPING */
-    const handleScraping = () => {
-        setStatus('isScraping');
-        const emails = fetchEmails();
-        /**CHECK CONSOLE */
-        console.log(emails);
-    };
-
-    /**INITIALIZE CLIENT TO USE GAPI */
-    const initClient = async () => {
-        try {
-            await window.gapi.client.init({
-                clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-                discoveryDocs: [
-                    'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest',
-                ],
-                scope: 'https://www.googleapis.com/auth/gmail.readonly',
-            });
-
-            const isSignedIn = window.gapi.auth2
-                .getAuthInstance()
-                .isSignedIn.get();
-
-            if (isSignedIn) {
-                window.gapi.auth2.getAuthInstance().signOut();
-            }
-
-            // Listen for sign-in state changes.
-            window.gapi.auth2.getAuthInstance().isSignedIn.listen((success) => {
-                if (success) {
-                    handleScraping();
-                    //HANDLE FECTH EMAILS
-                }
-            });
-        } catch (error) {
-            console.log(JSON.stringify(error, null, 2));
-        }
-    };
-
-    useEffect(() => {
-        setTimeout(() => {
-            setStatus('notAuthorized');
-        }, 3000);
-    }, []);
-
-    //INITIALIZE GOOGLE API
-    useEffect(() => {
-        window.onGoogleScriptLoad = () => {
-            window.gapi.load('client:auth2', initClient);
-        };
-
-        loadGoogleScript();
-    }, []);
-
-    return (
-        <div id="DashboardInitial">
-            {status === 'notAuthorized' && (
-                <Authorize triggerScanNow={triggerScanNow} />
-            )}
-            {status === 'isScraping' && <Scanning></Scanning>}
-            {(status === 'isAuthorizing' || status === '') && (
-                <div>
-                    <Spinner size="lg" color="#570097" animation="border" />
-                </div>
-            )}
+  return (
+    <div id="DashboardInitial">
+      {status === 'notAuthorized' && (
+        <Authorize triggerScanNow={triggerScanNow} />
+      )}
+      {status === 'isScraping' && <Scanning></Scanning>}
+      {(status === 'isAuthorizing' || status === '') && (
+        <div>
+          <Spinner size="lg" color="#570097" animation="border" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default DashboardPageInitial;
