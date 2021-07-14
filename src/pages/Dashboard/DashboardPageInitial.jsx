44b3322a726e0c31a7a3ff138c35ebe1a80d49bb
Adewusi Victor <glove.adewusi@gmail.com>
2021-07-14 11:52:23 +0100

    Add generic error notification when error occurs in popup

--- src/pages/Dashboard/DashboardPageInitial.jsx
+++ src/pages/Dashboard/DashboardPageInitial.jsx
@@ -172,7 +172,18 @@ const DashboardPageInitial = () => {
             </div>
           ),
         });
+        return;
       }
+      showError({
+        message: (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <AlertCircle />
+            <h4 className="ml-3 mb-0" style={{ lineHeight: '16px' }}>
+              Error! An error occurred.
+            </h4>
+          </div>
+        ),
+      });
     }
   };
 
