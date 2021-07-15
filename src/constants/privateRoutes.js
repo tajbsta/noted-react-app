53349a05cc554a78bb826c38c747b39617d4acf5
Adewusi Victor <glove.adewusi@gmail.com>
2021-07-15 13:20:09 +0100

    Add scraper to existing dashboard route and make adjustments for toast container and top navbar

--- src/constants/privateRoutes.js
+++ src/constants/privateRoutes.js
@@ -1,19 +1,17 @@
 import AuthorizePage from '../pages/AuthorizePage';
 import ProfilePage from '../pages/Profile/ProfilePage';
 import SettingsPage from '../pages/Settings/SettingsPage';
-import DashboardPage from '../pages/Dashboard/DashboardPage';
 import { CheckoutPage } from '../pages/Order/CheckoutPage';
 import { ViewOrderPage } from '../pages/Order/ViewOrderPage';
 import DashboardPageInitial from '../pages/Dashboard/DashboardPageInitial';
 // import EditOrderPage from '../pages/EditOrder/EditOrder';
 
 export default [
-    { path: '/request-permission', component: AuthorizePage },
-    { path: '/dashboard', component: DashboardPage },
-    { path: '/dashboard/initial', component: DashboardPageInitial },
-    { path: '/checkout', component: CheckoutPage },
-    { path: '/profile', component: ProfilePage },
-    { path: '/settings', component: SettingsPage },
-    { path: '/order/:id', component: ViewOrderPage },
-    // { path: '/edit-order', component: EditOrderPage },
+  { path: '/request-permission', component: AuthorizePage },
+  { path: '/dashboard', component: DashboardPageInitial },
+  { path: '/checkout', component: CheckoutPage },
+  { path: '/profile', component: ProfilePage },
+  { path: '/settings', component: SettingsPage },
+  { path: '/order/:id', component: ViewOrderPage },
+  // { path: '/edit-order', component: EditOrderPage },
 ];
