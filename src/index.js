bc00eb279d592369811b449049e6d72d73e75ba7
Adewusi Victor <glove.adewusi@gmail.com>
2021-08-12 17:12:08 +0100

    remove ghost comma from ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import Amplify from 'aws-amplify'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
@@ -19,16 +19,16 @@ Amplify.configure({
       responseType: 'code',
     },
   },
-})
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />,
+    <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-)
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
