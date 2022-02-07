ec98d45e07206bed8a0d58b6a125b8e7d2ca1eb6
jmdmacapagal <jmdmacapagal@gmail.com>
2022-02-07 22:05:50 +0800

    add console log

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -211,6 +211,7 @@ export default function DashboardPage({ triggerScanNow }) {
 
       if (user && !user['custom:stripe_sub_name']) {
         await subscribeUserToRuby(true);
+        console.log('in dashboard page');
       }
     })();
   }, []);
