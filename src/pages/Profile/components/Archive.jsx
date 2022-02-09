839c236b0cef4672431c7146b6fac322b8f077d7
jmdmacapagal <jmdmacapagal@gmail.com>
2022-02-09 19:26:06 +0800

    unarchive items

--- src/pages/Profile/components/Archive.jsx
+++ src/pages/Profile/components/Archive.jsx
@@ -2,9 +2,70 @@ import React, { useState, useEffect } from 'react';
 import Collapsible from 'react-collapsible';
 import ArchivedItem from './ArchivedItem';
 import { getProducts } from '../../../api/productsApi';
+import { timeout } from '../../../utils/time';
+import { isEmpty } from 'lodash-es';
+import { ProgressBar } from 'react-bootstrap';
+import { AlertCircle } from 'react-feather';
+import { showError } from '../../../library/notifications.library';
+import { toggleArchiveItem } from '../../../api/productsApi';
 
 export default function Archive({ user }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [archivedItems, setArchivedItems] = useState(null);
+  const [lastEvaluatedKey, setLastEvaluatedKey] = useState(null);
+  const [loadProgress, setLoadProgress] = useState(0);
+  const [fetchingOrders, setFetchingOrders] = useState(false);
+  const [isItemUnArchived, setIsItemunArchived] = useState(false);
+
+  const onArchive = (id) => {
+    toggleArchiveItem({ _id: id, isArchived: false });
+    setIsItemunArchived(true);
+  };
+
+  const fetchArchivedItems = async (nextPageKey = null) => {
+    const params = {
+      userId: user.sub,
+      sortBy: 'updated_at,_id',
+      sort: 'asc,asc',
+      isArchived: true,
+    };
+
+    try {
+      setFetchingOrders(true);
+
+      setLoadProgress(20);
+      await timeout(200);
+      setLoadProgress(35);
+      await timeout(200);
+      setLoadProgress(65);
+
+      const products = await getProducts(params);
+      setArchivedItems(products);
+
+      setLoadProgress(80);
+      await timeout(200);
+      setLoadProgress(100);
+      await timeout(1000);
+      /**
+       * Give animation some time
+       */
+      setTimeout(() => {
+        setFetchingOrders(false);
+      }, 600);
+    } catch (error) {
+      showError({
+        message: (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <AlertCircle />
+            <h4 className='ml-3 mb-0' style={{ lineHeight: '16px' }}>
+              Error getting order history!
+            </h4>
+          </div>
+        ),
+      });
+      setFetchingOrders(false);
+    }
+  };
 
   const renderTrigger = () => {
     return (
@@ -18,25 +79,33 @@ export default function Archive({ user }) {
     );
   };
 
-  const fetchArchivedItems = async () => {
-    const params = {
-      userId: user.sub,
-      size: 5,
-      sortBy: 'updated_at,_id',
-      sort: 'asc,asc',
-      isArchived: true,
-    };
-    try {
-      const products = await getProducts(params);
-      console.log(products);
-    } catch (error) {
-      console.log(error);
-    }
+  const renderEmptiness = () => {
+    return (
+      <>
+        <h3 className='sofia pro empty-message mt-5'>No returns yet!</h3>
+        <h5 className='sofia pro empty-submessage mb-5'>
+          I&apos;m sorry {(user && user.name) || (user && user.email)}, I&apos;m
+          afraid there&apos;s nothing here. Change that by {''}
+          <a href='/dashboard' className='start-returning'>
+            start returning.
+          </a>
+        </h5>
+      </>
+    );
+  };
+
+  const showMore = () => {
+    // if (lastEvaluatedKey) {
+    //   getOrderHistory(lastEvaluatedKey);
+    // } else {
+    //   setLastEvaluatedKey(null);
+    // }
   };
 
   useEffect(() => {
     fetchArchivedItems();
-  }, []);
+    setIsItemunArchived(false);
+  }, [isItemUnArchived]);
 
   return (
     <Collapsible
@@ -46,30 +115,33 @@ export default function Archive({ user }) {
       onTriggerClosing={() => setIsOpen(false)}
       trigger={renderTrigger()}
     >
-      <ArchivedItem />
-      {/* {!fetchingOrders && isEmpty(orders) && renderEmptiness()}
-      {!isEmpty(orders) &&
-        orders.map((order) => (
-          <ReturnHistoryItem order={order} key={order.id} />
-        ))} */}
-      {/* {fetchingOrders && (
-          <ProgressBar
-            animated
-            striped
-            now={loadProgress}
-            className='m-bar mt-4 m-3'
+      {!fetchingOrders && isEmpty(archivedItems) && renderEmptiness()}
+      {!isEmpty(archivedItems) &&
+        archivedItems.map((item) => (
+          <ArchivedItem
+            archivedItem={item}
+            key={item.id}
+            onArchive={onArchive}
           />
-        )} */}
-      {/* {lastEvaluatedKey && (
-          <div className='d-flex justify-content-center'>
-            <button
-              className='sofia-pro btn btn-show-more noted-purple'
-              onClick={showMore}
-            >
-              Show more
-            </button>
-          </div>
-        )} */}
+        ))}
+      {fetchingOrders && (
+        <ProgressBar
+          animated
+          striped
+          now={loadProgress}
+          className='m-bar mt-4 m-3'
+        />
+      )}
+      {lastEvaluatedKey && (
+        <div className='d-flex justify-content-center'>
+          <button
+            className='sofia-pro btn btn-show-more noted-purple'
+            onClick={showMore}
+          >
+            Show more
+          </button>
+        </div>
+      )}
     </Collapsible>
   );
 }
--- src/pages/Profile/components/ArchivedItem.jsx
+++ src/pages/Profile/components/ArchivedItem.jsx
@@ -1,36 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { Accordion, Card, Col, Row } from 'react-bootstrap';
-import NikeIcon from '../../../assets/icons/nike-sample-icon.svg';
 import ReturnIcon from '../../../assets/icons/return-arrow-icon.svg';
-import SampleProduct from '../../../assets/img/sample-product.png';
+import ProductPlaceholder from '../../../assets/img/ProductPlaceholder.svg';
+import { toTitleCase } from '../../../utils/data';
 
-export default function ArchivedItem() {
+export default function ArchivedItem({ archivedItem, onArchive }) {
   const [isMobile, setIsMobile] = useState(false);
+  const { _id, name, vendor, price, thumbnail, vendor_data } = archivedItem;
+
+  // Truncate name if longer than 45 characters
+  const truncateProductNameInDesktop = (str, num = 35) => {
+    if (str && str.length > num) {
+      return str.slice(0, num) + '...';
+    } else {
+      return str;
+    }
+  };
+
+  const formattedProductName = toTitleCase(name);
+  const truncatedName = truncateProductNameInDesktop(formattedProductName);
+
   return (
-    <div className='row history-row' key=''>
+    <div className='row'>
       <Card className={`col mt-1 shadow-sm ${isMobile ? 'ml-0' : 'ml-4 m-3'}`}>
         <Card.Body className='py-3 px-4'>
           {!isMobile && (
             <div className='flex row align-items-center justify-content-between'>
               <Col sm={6} className='flex row'>
                 <img
-                  src={SampleProduct}
+                  src={thumbnail}
                   className='mr-3'
-                  style={{ width: 60, height: 60 }}
+                  style={{
+                    border: '1px solid #f0f0f0',
+                    borderRadius: '8px',
+                    justifyContent: 'center',
+                    width: '48px',
+                    height: '48px',
+                    background: '#fff',
+                    objectFit: 'contain',
+                  }}
                 />
 
                 <div>
                   <strong className='my-0' style={{ fontSize: 13 }}>
-                    Nike
+                    {truncatedName}
                   </strong>
                   <p
                     className='my-0'
                     style={{ fontSize: 13, color: '#2E1D3A' }}
                   >
-                    Metcon 6
+                    {vendor}
                   </p>
                   <strong className='my-0' style={{ fontSize: 13 }}>
-                    $130.00{' '}
+                    ${price}{' '}
                   </strong>
                   <span className='' style={{ fontSize: 12, color: '#0E0018' }}>
                     Cash back
@@ -45,8 +67,30 @@ export default function ArchivedItem() {
                   src={ReturnIcon}
                   alt='icon'
                   style={{ cursor: 'pointer' }}
+                  onClick={() => onArchive(_id)}
                 />
-                <img src={NikeIcon} alt='icon' className='ml-3' />
+                <div>
+                  <img
+                    src={
+                      (vendor_data && vendor_data.thumbnail) ||
+                      ProductPlaceholder
+                    }
+                    onError={(e) => {
+                      e.currentTarget.src = ProductPlaceholder;
+                    }}
+                    alt=''
+                    style={{
+                      border: '1px solid #f0f0f0',
+                      borderRadius: '8px',
+                      justifyContent: 'center',
+                      width: '48px',
+                      height: '48px',
+                      background: '#fff',
+                      objectFit: 'contain',
+                      marginLeft: 10,
+                    }}
+                  />
+                </div>
               </Col>
             </div>
           )}
