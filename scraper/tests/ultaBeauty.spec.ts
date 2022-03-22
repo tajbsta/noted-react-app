427e0e4dc4a4c107dfb7d6952a0f217ebbacd2a8
jmdmacapagal <jmdmacapagal@gmail.com>
2022-03-22 18:29:18 +0800

    removed only

--- scraper/tests/ultaBeauty.spec.ts
+++ scraper/tests/ultaBeauty.spec.ts
@@ -16,7 +16,7 @@ moment.tz.setDefault('Etc/UTC');
 
 const TEST_DATA_URL = 'https://noted-scrape-test.s3.us-west-2.amazonaws.com/ULTABEAUTY.json';
 
-describe.only('Ulta Beauty', () => {
+describe('Ulta Beauty', () => {
   let sandbox: sinon.SinonSandbox;
   let payload: IEmailPayload = {
     raw: '',
